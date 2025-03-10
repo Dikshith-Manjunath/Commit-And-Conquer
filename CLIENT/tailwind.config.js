10258ce05a6c22cfad4494417fae351c2554fcee
Dikshith <dikshithrrddy@gmail.com>
2025-03-10 11:20:38 +0530

    Dark Mode Enabled

---
+++ CLIENT/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class', // Enables dark mode via a class
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}", // Make sure Tailwind scans your files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
